2bc1ca5f9921d3e26b00bb87a197e2c4f9952432
eric <eli@vendasta.com>
2020-03-08 21:36:49 -0600

    update project setting

--- src/app/body/project/project.interface.ts
+++ src/app/body/project/project.interface.ts
@@ -2,6 +2,8 @@ export interface ProjectList {
   id: number;
   name: string;
   languages: string;
+  mini_describe: string;
+  describe: string;
   level: string;
   date: Date;
 }
--- src/app/body/project/project.model.ts
+++ src/app/body/project/project.model.ts
@@ -5,6 +5,9 @@ export const Project: ProjectList[] = [
     id: 1,
     name: 'Personal Website',
     languages: 'Angular',
+    mini_describe: 'Designed interactive website with responsive front-end features allowing students to provide academic feedback.',
+    // tslint:disable-next-line:max-line-length
+    describe: 'Designed interactive website with responsive front-end features allowing students to provide academic feedback.\n● Built interactive website framework using JavaScript, CSS, and HTML allowing students to provide numeric ratings and qualitative comments on course curriculum and faculty.\n● Implemented localized faculty and course record data by integrating institutional database into web design.',
     level: 'high',
     date: new Date('02/02/2020')
   },
@@ -12,6 +15,11 @@ export const Project: ProjectList[] = [
     id: 2,
     name: 'Food Order System',
     languages: 'Java',
+    mini_describe: 'Designed interactive website with responsive front-end features allowing students to provide academic feedback.',
+    describe: 'Designed interactive website with responsive front-end features allowing students to provide academic feedback.\n' +
+      // tslint:disable-next-line:max-line-length
+      '● Built interactive website framework using JavaScript, CSS, and HTML allowing students to provide numeric ratings and qualitative comments on course curriculum and faculty.\n' +
+      '● Implemented localized faculty and course record data by integrating institutional database into web design.',
     level: 'high',
     date: new Date('22/01/2018')
   },
@@ -19,6 +27,11 @@ export const Project: ProjectList[] = [
     id: 2,
     name: 'Flight Booking System',
     languages: 'Java',
+    mini_describe: 'Designed interactive website with responsive front-end features allowing students to provide academic feedback.',
+    describe: 'Designed interactive website with responsive front-end features allowing students to provide academic feedback.\n' +
+      // tslint:disable-next-line:max-line-length
+      '● Built interactive website framework using JavaScript, CSS, and HTML allowing students to provide numeric ratings and qualitative comments on course curriculum and faculty.\n' +
+      '● Implemented localized faculty and course record data by integrating institutional database into web design.',
     level: 'high',
     date: new Date('22/01/2018')
   },
@@ -26,6 +39,11 @@ export const Project: ProjectList[] = [
     id: 2,
     name: 'Passwords Container',
     languages: 'Java',
+    mini_describe: 'Designed interactive website with responsive front-end features allowing students to provide academic feedback.',
+    describe: 'Designed interactive website with responsive front-end features allowing students to provide academic feedback.\n' +
+      // tslint:disable-next-line:max-line-length
+      '● Built interactive website framework using JavaScript, CSS, and HTML allowing students to provide numeric ratings and qualitative comments on course curriculum and faculty.\n' +
+      '● Implemented localized faculty and course record data by integrating institutional database into web design.',
     level: 'high',
     date: new Date('22/01/2018')
   },
@@ -33,6 +51,11 @@ export const Project: ProjectList[] = [
     id: 2,
     name: 'what ever it is',
     languages: 'Java',
+    mini_describe: 'Designed interactive website with responsive front-end features allowing students to provide academic feedback.',
+    describe: 'Designed interactive website with responsive front-end features allowing students to provide academic feedback.\n' +
+      // tslint:disable-next-line:max-line-length
+      '● Built interactive website framework using JavaScript, CSS, and HTML allowing students to provide numeric ratings and qualitative comments on course curriculum and faculty.\n' +
+      '● Implemented localized faculty and course record data by integrating institutional database into web design.',
     level: 'high',
     date: new Date('22/01/2018')
   },
