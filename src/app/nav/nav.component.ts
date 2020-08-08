3c6553ac761c35ff6c3b05d2ebdab9a557932faf
Eric <ericworkuse@gmail.com>
2020-08-08 14:09:37 -0600

    Update nav menubar

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {MenuModule} from 'primeng/menu';
 import {MenuItem} from 'primeng/api';
-import {MegaMenuItem} from 'primeng/api';  // required when using MegaMenu
 
 @Component({
   selector: 'app-nav',
@@ -9,122 +7,18 @@ import {MegaMenuItem} from 'primeng/api';  // required when using MegaMenu
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-  items: MegaMenuItem[];
+  items: MenuItem[];
+  activeItem: MenuItem;
   constructor() { }
 
   ngOnInit(): void {
     this.items = [
-      {
-        label: 'Videos', icon: 'pi pi-fw pi-video',
-        items: [
-          [
-            {
-              label: 'Video 1',
-              items: [{label: 'Video 1.1'}, {label: 'Video 1.2'}]
-            },
-            {
-              label: 'Video 2',
-              items: [{label: 'Video 2.1'}, {label: 'Video 2.2'}]
-            }
-          ],
-          [
-            {
-              label: 'Video 3',
-              items: [{label: 'Video 3.1'}, {label: 'Video 3.2'}]
-            },
-            {
-              label: 'Video 4',
-              items: [{label: 'Video 4.1'}, {label: 'Video 4.2'}]
-            }
-          ]
-        ]
-      },
-      {
-        label: 'Users', icon: 'pi pi-fw pi-users',
-        items: [
-          [
-            {
-              label: 'User 1',
-              items: [{label: 'User 1.1'}, {label: 'User 1.2'}]
-            },
-            {
-              label: 'User 2',
-              items: [{label: 'User 2.1'}, {label: 'User 2.2'}]
-            },
-          ],
-          [
-            {
-              label: 'User 3',
-              items: [{label: 'User 3.1'}, {label: 'User 3.2'}]
-            },
-            {
-              label: 'User 4',
-              items: [{label: 'User 4.1'}, {label: 'User 4.2'}]
-            }
-          ],
-          [
-            {
-              label: 'User 5',
-              items: [{label: 'User 5.1'}, {label: 'User 5.2'}]
-            },
-            {
-              label: 'User 6',
-              items: [{label: 'User 6.1'}, {label: 'User 6.2'}]
-            }
-          ]
-        ]
-      },
-      {
-        label: 'Events', icon: 'pi pi-fw pi-calendar',
-        items: [
-          [
-            {
-              label: 'Event 1',
-              items: [{label: 'Event 1.1'}, {label: 'Event 1.2'}]
-            },
-            {
-              label: 'Event 2',
-              items: [{label: 'Event 2.1'}, {label: 'Event 2.2'}]
-            }
-          ],
-          [
-            {
-              label: 'Event 3',
-              items: [{label: 'Event 3.1'}, {label: 'Event 3.2'}]
-            },
-            {
-              label: 'Event 4',
-              items: [{label: 'Event 4.1'}, {label: 'Event 4.2'}]
-            }
-          ]
-        ]
-      },
-      {
-        label: 'Settings', icon: 'pi pi-fw pi-cog',
-        items: [
-          [
-            {
-              label: 'Setting 1',
-              items: [{label: 'Setting 1.1'}, {label: 'Setting 1.2'}]
-            },
-            {
-              label: 'Setting 2',
-              items: [{label: 'Setting 2.1'}, {label: 'Setting 2.2'}]
-            },
-            {
-              label: 'Setting 3',
-              items: [{label: 'Setting 3.1'}, {label: 'Setting 3.2'}]
-            }
-          ],
-          [
-            {
-              label: 'Technology 4',
-              items: [{label: 'Setting 4.1'}, {label: 'Setting 4.2'}]
-            }
-          ]
-        ]
-      }
-    ]
+      {label: 'Home', icon: 'pi pi-fw pi-home'},
+      {label: 'Project', icon: 'pi pi-fw pi-th-large'},
+      {label: 'Contact Me', icon: 'pi pi-fw pi-id-card'},
+    ];
+
+    this.activeItem = this.items[0];
   }
 
 }
